fdd3333a7f45644a9b3b843584c7a766fefc5e24
Jynix0614 <727342289@qq.com>
2021-11-23 14:25:16 +0800

    L859 AC

--- src/L859_BuddyStrings.java
+++ src/L859_BuddyStrings.java
@@ -1,9 +1,33 @@
 public class L859_BuddyStrings {
     public static void main(String[] args) {
-
+        String s= "aaaaaacb";
+        String goal = "aaaaaabc";
+        System.out.println(buddyStrings(s,goal));
     }
 
     public static boolean buddyStrings(String s, String goal) {
-        return false;
+        if (s.length() != goal.length()){
+            return false;
+        }
+        if (s.equals(goal)){
+           int[] count = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                if (count[s.charAt(i) - 'a'] > 1){
+                    return true;
+                }
+            }
+            return false;
+        }else {
+            int first = -1, second = -1;
+            for (int i = 0; i < goal.length(); i++) {
+                if (s.charAt(i) != goal.charAt(i)){
+                    if (first == -1) first = i;
+                    else if (second == -1) second = i;
+                    else return false;
+                }
+            }
+            return  (second != -1 && s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(second));
+        }
     }
 }
